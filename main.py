7b2eb4013c9546171cbfa659785df5474adb02a1
Niranjan <niranjansaikoppisetti@gmail.com>
2025-05-29 13:26:40 +0530

    29 May, 1:30 pm

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-print("hii")
+from jinja2.compiler import generate
+from typing import Set
+from backend.core import run_llm
+import streamlit as st
 
+
+st.header("Document Helper BOT")
+
+
+def create_sources_string(source_urls: Set[str]) -> str:
+    if not source_urls:
+        return ""
+    sources_list = list(source_urls)
+    sources_list.sort()
+    sources_string = "sources:\n"
+    for i, source in enumerate(sources_list):
+        sources_string += f"{i+1}. {source}\n"
+    return sources_string
+
+prompt = st.text_input("User Prompt", placeholder="Enter your query....")
+if prompt:
+    with st.spinner("Generating response ... "):
+        generated_response = run_llm(query=prompt)
+        sources = set([doc.metadata["source"] for doc in generated_response["source_documents"]])
+
+        formatted_response = (
+            f"{generated_response['result']} \n\n {create_sources_string(sources)}"
+        )
 
 
 
